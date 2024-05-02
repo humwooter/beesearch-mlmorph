f19e7886a0cbced7ae260c969393cb1aafd7b361
Katya Raman <katyaraman@icloud.com>
2024-05-02 01:05:11 -0700

    prediction results with feauture pool size=800, num trees=800

--- run_ml_morph.sh
+++ run_ml_morph.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # variables - update these with your specific paths
-IMAGE_DIR="../tpsdig/Wing Images/Agapostemon texanus"
-TPS_FILE="../tpsdig/Wing TPS/Agapostemon texanus.tps"
+# IMAGE_DIR="../tpsdig/Wing Images/Agapostemon texanus"
+# TPS_FILE="../tpsdig/Wing TPS/Agapostemon texanus.tps"
+
+# IMAGE_DIR="../tpsdig/Wing Images/Apis mellifera"
+# TPS_FILE="../tpsdig/Wing TPS/Apis mellifera.tps"
+
+# IMAGE_DIR="../tpsdig/Wing Images/Bombus californicus"
+# TPS_FILE="../tpsdig/Wing TPS/Bombus californicus.tps"
+
+IMAGE_DIR="../tpsdig/Wing Images/Bombus vosnesenskii"
+TPS_FILE="../tpsdig/Wing TPS/Bombus vosnesenskii.tps"
+
+# IMAGE_DIR="../tpsdig/Wing Images/Xylocopa tabaniformis orpifex"
+# TPS_FILE="../tpsdig/Wing TPS/Xylocopa tabaniformis orpifex.tps"
 
 # IMAGE_DIR="./image-examples"
 # TPS_FILE="./landmark-examples/tps-example.tps"
@@ -12,20 +24,23 @@ TPS_FILE="../tpsdig/Wing TPS/Agapostemon texanus.tps"
 # TPS_FILE="../bee-image-data/Images/TPS files/all tripartitus TPS files (to append)/set14_29sep2020.tps"
 
 
-# step 1: preprocessing
-# splitting images into train and test sets, generating xml files
-echo "running preprocessing..."
-python3 preprocessing.py -i "$IMAGE_DIR" -t "$TPS_FILE"
+
 
 # # Visualize TPS coordinates on images before training
 # echo "Visualizing TPS coordinates on images..."
 # python3 visualize_tps_on_images.py -i "$IMAGE_DIR" -t "$TPS_FILE" -o "./training_landmarked_images"
+
+
 
 # Calculate ideal window size
 echo "Calculating ideal window size..."
 WINDOW_SIZE=$(python3 calculate_window_size.py "$TPS_FILE")
 echo "Ideal window size calculated: $WINDOW_SIZE"
 
+# step 1: preprocessing
+# splitting images into train and test sets, generating xml files
+echo "running preprocessing..."
+python3 preprocessing.py -i "$IMAGE_DIR" -t "$TPS_FILE"
 
 
 # step 2: training object detectors
@@ -37,7 +52,7 @@ python3 detector_trainer.py -d train.xml -t test.xml -n 8 -w $WINDOW_SIZE -e 0.0
 # training the shape predictor model
 echo "training shape predictor..."
 # runs shape training with 8 threads, tree depth 5, cascade depth 20, nu 0.08, oversampling 200, 25 test splits, feature pool size 700, and 400 trees
-python3 shape_trainer.py -d train.xml -t test.xml -th 8 -dp 5 -c 20  -nu 0.08 -os 200 -s 25 -f 700 -n 400
+python3 shape_trainer.py -d train.xml -t test.xml -th 8 -dp 5 -c 20  -nu 0.08 -os 200 -s 25 -f 800 -n 800
 
 # step 4: predicting landmarks
 # predicting landmarks on new images
